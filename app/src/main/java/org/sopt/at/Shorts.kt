c3af6a670ecc5a88f9bd38c0ced42ff1c4081418
suri <quffl2002@gmail.com>
2025-04-25 23:00:35 +0900

    바텀 네비바 클릭시 각 페이지로 이동

---
+++ app/src/main/java/org/sopt/at/Shorts.kt
@@ -0,0 +1,35 @@
+package org.sopt.at
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
+
+@Composable
+fun Shorts(modifier: Modifier = Modifier){
+    Text(
+        text = "Shorts",
+        color = Color.Black,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(start = 4.dp, bottom = 8.dp, top = 24.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview6() {
+    Shorts()
+}
