80dc8bdac2821e6ae9d4927adcac6aec007dd198
suri <quffl2002@gmail.com>
2025-05-02 16:44:56 +0900

    Color.kt 설정

--- app/src/main/java/org/sopt/at/ui/theme/Theme.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Theme.kt
@@ -12,31 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun ATSOPTANDROIDTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -51,8 +34,8 @@ fun ATSOPTANDROIDTheme(
     }
 
     MaterialTheme(
+        typography = AppTypography,
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
